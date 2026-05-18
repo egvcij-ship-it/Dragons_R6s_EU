ccf7ee679d0aad4a546480c22df52459f1bd8de9
EGV-MaraSuitt_PZ <egvcij@gmail.com>
2026-05-18 09:14:22 +0200

    Update match results and dates in matches-data.js

--- matches-data.js
+++ matches-data.js
@@ -15,15 +15,15 @@
 const matchesData = [
    {
     opponent: "Euskal", opponentLogo: "Logos/euskal.png",
-    ourScore: 0, opponentScore: 0, status: "upcoming",
+    ourScore: 7, opponentScore: 5, status: "win",
     league: "kom", leagueName: "King of Master",
-    leagueLogo: "kom.png", date: "- MAY, 2026"
+    leagueLogo: "kom.png", date: "16 MAY, 2026"
   },
   {
     opponent: "ProdigiFive", opponentLogo: "Logos/p5.png",
-    ourScore: 0, opponentScore: 0, status: "upcoming",
+    ourScore: 0, opponentScore: 2, status: "loss",
     league: "esp", leagueName: "Competición Española",
-    leagueLogo: "competicion_española.png", date: "- MAY, 2026"
+    leagueLogo: "competicion_española.png", date: "17 MAY, 2026"
   },
   {
     opponent: "S.O.T", opponentLogo: "Logos/sot.png",
